629291764116dc7e29b7dfb2efb38e7ec123034b
Thomas Nichols <ta.nichols@outlook.com>
2017-12-10 15:24:48 -0600

    Added heroku app in auth config

--- src/app/auth/auth-config.ts
+++ src/app/auth/auth-config.ts
@@ -10,6 +10,6 @@ export const AUTH_CONFIG: AuthConfig = {
   CLIENT_ID: 'w187UHEwhB4iDsQSEZMYN9IG8yPsocGL',
   CLIENT_DOMAIN: 'tanichols.auth0.com', // e.g., you.auth0.com
   AUDIENCE: 'https://tanichols.auth0.com/userinfo',
-  REDIRECT: 'http://localhost:4200/callback',
+  REDIRECT: 'https://coinvault-client.herokuapp.com/callback',
   SCOPE: 'openid profile email'
 };
