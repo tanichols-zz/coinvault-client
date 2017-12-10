4bc9aab8d76c5d027e7bcc3f041b616593bb4fcc
Thomas Nichols <ta.nichols@outlook.com>
2017-12-10 16:16:19 -0600

    Added logger

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express')
-const app = express()
 var path = require('path');
+var morgan = require('morgan')
+
+const app = express()
 
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'))
 
-// For all GET requests, send back index.html
+app.use(morgan('combined'))
+
+// For all requests, send back index.html
 // so that PathLocationStrategy can be used
 app.all('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
